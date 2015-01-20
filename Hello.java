aa242d693718491dcf7a1771d833ed8f0b533948
ben <ben_hh@sina.com>
2015-01-20 12:30:14 +0800

    Update Hello.java File .Add Two Lines

--- Hello.java
+++ Hello.java
@@ -1,5 +1,7 @@
 public class Hello {
 	public static void main(String[] args) {
 		System.out.println("Hello  World!");
+                System.out.println("Hello  WuHan!");
+                System.out.println("Hello  XuXin!");
 	}
 }
