3a0cd3b80024dcf32cdc72bffbaa82e8f8fc938a
ben <ben_hh@sina.com>
2015-01-20 12:55:39 +0800

    Add Demo.java File

--- Hello.java
+++ Hello.java
@@ -1,7 +1,5 @@
 public class Hello {
 	public static void main(String[] args) {
-		System.out.println("Hello  World!");
-                System.out.println("Hello  WuHan!");
-                System.out.println("Hello  XuXin!");
+		System.out.println("www.nx139.com");
 	}
 }
