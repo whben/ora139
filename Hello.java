096d203508efe293570515fc25e5fa0f403f216c
ben <ben_hh@sina.com>
2015-01-21 10:39:54 +0800

    simple print

--- Hello.java
+++ Hello.java
@@ -1,7 +1,5 @@
 public class Hello {
-    public static final String COMPANY_NAME = "ben";	
     public static void main(String[] args) {
-		System.out.println("www.haha.com");
-                System.out.println("www.baba.com");
-                System.out.println();
+		System.out.println("********");
+                System.out.println("########");
 }
