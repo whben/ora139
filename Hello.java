38593a0f6e048d18b6d904b77cce4743f2e7d6bb
ben <ben_hh@sina.com>
2015-01-21 09:18:24 +0800

    Modified Hello.java

--- Hello.java
+++ Hello.java
@@ -1,5 +1,5 @@
 public class Hello {
 	public static void main(String[] args) {
-		System.out.println("www.139.com");
-	}
+		System.out.println("www.haha.com");
+                System.out.println("www.baba.com");
 }
