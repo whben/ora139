b2c4b3d64188a5f3e37aa2257233aa5dc3eebf32
ben <ben_hh@sina.com>
2015-01-21 10:42:59 +0800

    hello print

--- Hello.java
+++ Hello.java
@@ -1,5 +1,5 @@
 public class Hello {
     public static void main(String[] args) {
-		System.out.println("********");
-                System.out.println("########");
+		System.out.println("Hello Wuhan");
+                System.out.println("Hello Beijing");
 }
