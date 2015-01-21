fcc1493df1140d98b1dfb462262c3a43ddc86560
ben <ben_hh@sina.com>
2015-01-21 10:46:34 +0800

    url print

--- Hello.java
+++ Hello.java
@@ -1,5 +1,6 @@
 public class Hello {
     public static void main(String[] args) {
-		System.out.println("********");
-                System.out.println("########");
+		System.out.println("www.138.com");
+                System.out.println("www.139.com");
+                System.out.println("www.140.com");
 }
