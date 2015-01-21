8e7bc74f8298281273b87111f3e55ef6185a5444
ben <ben_hh@sina.com>
2015-01-21 10:27:24 +0800

    Add Staitc Attribute

--- Hello.java
+++ Hello.java
@@ -2,4 +2,5 @@ public class Hello {
 	public static void main(String[] args) {
 		System.out.println("www.haha.com");
                 System.out.println("www.baba.com");
+                System.out.println();
 }
